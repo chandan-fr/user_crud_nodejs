c7e94c901a54f6a65631b95c9c4385e14d39e78c
chandan-fr <georgesteve31019@gmail.com>
2024-01-03 18:51:48 +0530

    modified file name and extname added

--- config/mediaConfig.js
+++ config/mediaConfig.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         const midName = file.originalname.split(".")[0];
-        cb(null, file.fieldname + '-' + midName + '-' + uniqueSuffix);
+        cb(null, midName + '-' + uniqueSuffix + path.extname(file.originalname));
     }
 })
 
