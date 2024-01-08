4a199a97cd350c0201ab6f586236e7d83ca9f31f
chandan-fr <georgesteve31019@gmail.com>
2024-01-08 19:54:56 +0530

    user session auth added

---
+++ middleware/userAuth.js
@@ -0,0 +1,10 @@
+const jwt = require("jsonwebtoken");
+const secret_key = require("../config/secretKey");
+
+const jwtSessionAuth = async (req, res, next) => {
+    try {
+
+    } catch (exc) {
+
+    }
+}
